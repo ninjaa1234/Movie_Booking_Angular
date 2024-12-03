c45fe2ceff94b38408d760de023f08cfb0069b6b
ninjan.nambiar <niranjansnambiar.sudinam@gmail.com>
2024-12-03 18:49:19 +0530

    first commit

---
+++ MovieTicketBookingApplication-BackEnd/MTB-App/src/main/java/com/cg/mts/MovieFinalApplication.java
@@ -0,0 +1,13 @@
+package com.cg.mts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieFinalApplication.class, args);
+	}
+
+}
